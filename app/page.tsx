ccfc4892dc5ea868e4ffcea20a82ad385b861eb0
HyeonU-Jo <chw3448@gmail.com>
2023-04-14 17:00:11 +0900

    fix: mui => daisyui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
 export default function Home() {
-    return <></>;
+    return (
+        <>
+            <button className='btn'>안녕하세요</button>
+        </>
+    );
 }
