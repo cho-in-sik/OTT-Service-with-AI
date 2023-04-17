f7b412e07882734298f38580e01cae113d506115
HyeonU-Jo <chw3448@gmail.com>
2023-04-17 14:34:03 +0900

    fix: 레이아웃 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import Footer from "../components/footer";
-import Header from "../components/header";
-
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <div className="h-[720px]"></div>
-      <Footer />
-    </>
-  );
+    return (
+        <>
+            <div className='h-[720px]'></div>
+        </>
+    );
 }
