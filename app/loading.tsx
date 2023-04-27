845e4969a784f0e9eefdb5f59ed525541250f91f
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-04-27 19:12:10 +0900

    loading ui

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
 import Spinner from '@/components/common/spinner';
+import React from 'react';
+//@ts-ignore
 const Loading = () => {
-  return <Spinner />;
+  return (
+    <div className="w-[100vw] h-[100vh]">
+      <Spinner />
+    </div>
+  );
 };
 
 export default Loading;
