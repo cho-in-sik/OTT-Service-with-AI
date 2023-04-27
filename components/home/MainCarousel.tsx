bc75e3e299f68158d1ca4e78ee89c659c1b6fc3e
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-04-27 19:11:32 +0900

    define types

--- components/home/MainCarousel.tsx
+++ components/home/MainCarousel.tsx
@@ -3,29 +3,20 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 //@ts-ignore
 import Carousel from 'react-grid-carousel';
-interface Data {
-  id: string;
-  title: string;
-  overview: string;
-  posterUrl: string;
-}
+import { Movie } from '@/types/movie';
 
-interface Props {
-  data: Data[];
-}
-
-const MainCarousel = ({ data }: Props) => {
+const MainCarousel = ({ list }: { list: Movie[] }) => {
   return (
     <>
       <div className="h-[600px] mb-10 bg-gradient-to-r from-cyan-500 to-blue-500">
         <Carousel cols={1} rows={1} loop autoplay={2000} showDots hideArrow>
-          {data.map(({ posterUrl, title, id, overview }, idx) => {
+          {list.map(({ posterUrl, title, id, overview }, idx) => {
             return (
               <Carousel.Item key={idx}>
                 <div
                   className="flex justify-around w-full h-full ml-auto mr-auto "
                   key={id}
-                  id={id}
+                  id={String(id)}
                 >
                   <div className="mt-auto mb-auto text-white w-[40rem]">
                     <h1 className="mb-4 font-bold break-words text-7xl">
